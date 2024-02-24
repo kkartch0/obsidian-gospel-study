7c4a0cea6d8ae14672b7f26520dd8d16959b095d
Ky Kartchner <kartchnerky@gmail.com>
2024-02-23 19:22:29 -0700

    some progress

---
+++ scripture grabber.mjs
@@ -0,0 +1,33 @@
+import puppeteer from 'puppeteer';
+import clipboard from 'clipboardy';
+
+// const url = 'https://www.churchofjesuschrist.org/study/general-conference/2023/10/42freeman?lang=eng&id=p3-p4#p3';
+// const url = 'https://www.churchofjesuschrist.org/study/manual/come-follow-me-for-home-and-church-book-of-mormon-2024/001-conversion?lang=eng&id=p1#p1';
+(async () => {
+  const url = process.argv[2];
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+
+  await page.goto(url, { waitUntil: 'domcontentloaded' });
+
+  const pageTitle = await page.title();
+  const titleLink = `#### [${pageTitle}](${url})\n`;
+
+  let text = titleLink;
+
+  const paragraphs = await page.$$eval('p.active-item', paragraphs => paragraphs.map(p => p.innerHTML));
+  paragraphs.forEach((innerHTML, index) => {
+    innerHTML = innerHTML.replace("/study/", "https://churchofjesuschrist.org/study/");
+    text += `\n${innerHTML}\n`; 
+  });
+
+  const tag = "\n#study/general-conference/2023/10/42freeman";
+
+  text += tag;
+
+  clipboard.write(text);
+  console.log(text);
+
+  await browser.close();
+})();
+
