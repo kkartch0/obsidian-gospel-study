6e167f7f40a837a3d609a79553c066be2e710f56
Ky Kartchner <kartchnerky@gmail.com>
2024-03-24 12:18:03 -0600

    add another endToEndTestCase

--- __mocks__/obsidian.ts
+++ __mocks__/obsidian.ts
@@ -1,24 +1,23 @@
 import { RequestUrlResponse, RequestUrlResponsePromise } from "obsidian";
 import fs from 'fs';
-import { string } from "yaml/dist/schema/common/string";
 
 export function requestUrl(url: string): Promise<RequestUrlResponse> {
   const urlToHtmlFileMap: { [key: string]: string } = {
-    "https://www.churchofjesuschrist.org/study/scriptures/bofm/2-ne/22?lang=eng&id=2#p2": "2nephi22.2.html"
+    "https://www.churchofjesuschrist.org/study/scriptures/bofm/2-ne/22?lang=eng&id=2#p2": "2nephi22.2.html",
+    "https://www.churchofjesuschrist.org/study/manual/come-follow-me-for-home-and-church-book-of-mormon-2024/10?lang=eng&id=p8": "weRejoiceInChrist.html"
   };
 
   const htmlFileName = urlToHtmlFileMap[url];
   const htmlFileContent = fs.readFileSync(`__mocks__/${htmlFileName}`, 'utf8');
 
-  return new Promise((resolve, reject) => {
-
+  return new Promise((resolve) => {
     resolve({
       status: 0,
       headers: {},
       arrayBuffer: new ArrayBuffer(0),
       json: null,
       text: htmlFileContent,
-    });
+    } as RequestUrlResponse);
   });
 
 }
