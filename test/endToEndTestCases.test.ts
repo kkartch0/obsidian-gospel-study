73862fb670f5818d9830363b7e2f1c1f7fc3a96b
Ky Kartchner <kartchnerky@gmail.com>
2024-03-24 14:08:49 -0600

    update end to end to allow for using default formats

--- test/endToEndTestCases.test.ts
+++ test/endToEndTestCases.test.ts
@@ -1,7 +1,7 @@
 import fs from 'fs';
 import yaml from '../node_modules/yaml/dist';
 import { getStudyBlockTextFromUrl } from '../src/getStudyBlockTextFromUrl';
-import { DEFAULT_SETTINGS, GospelStudyPluginSettings } from '../src/gospelStudyPluginSettings';
+import { DEFAULT_SETTINGS, GospelStudyPluginSettings, STUDY_BLOCK_FORMAT_1, STUDY_BLOCK_FORMAT_2 } from '../src/gospelStudyPluginSettings';
 
 let fileNames: string[] = [];
 
@@ -22,13 +22,22 @@ describe('get study block text from url', () => {
             // Arrange
             const fileContents = fs.readFileSync(`./test/endToEndTestCases/${file}`, 'utf8');
             const parsedFile = yaml.parse(fileContents) as EndToEndTestCase;
-            const pluginSettings = { DEFAULT_SETTINGS, ...parsedFile.pluginSettings };
+
+            const lookupTable : { [key:string] : string }= {
+                "STUDY_BLOCK_FORMAT_1": STUDY_BLOCK_FORMAT_1,
+                "STUDY_BLOCK_FORMAT_2": STUDY_BLOCK_FORMAT_2,
+            };
+
+            parsedFile.pluginSettings.studyBlockFormat = (lookupTable[parsedFile.pluginSettings.studyBlockFormat] 
+                || parsedFile.pluginSettings.studyBlockFormat).trim();
+
+            const pluginSettings = { ...DEFAULT_SETTINGS, ...parsedFile.pluginSettings };
 
             // Act
             const result = await getStudyBlockTextFromUrl(parsedFile.pastedText, pluginSettings);
 
             // Assert
-            expect(result).toBe(parsedFile.expectedResult);
+            expect(result).toBe(parsedFile.expectedResult.trim());
         });
     });
 });
