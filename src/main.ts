0d04af3dbccaf496ba8015a9a72292dc40482b63
Ky Kartchner <kartchnerky@gmail.com>
2024-03-27 16:08:41 -0600

    add onEditorChange

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,12 @@ import { Plugin, Editor } from "obsidian";
 import { GospelStudyPluginSettingTab } from "./gospelStudyPluginSettingTab";
 import { DEFAULT_SETTINGS, GospelStudyPluginSettings } from "./gospelStudyPluginSettings";
 import { getStudyBlockTextFromUrl } from "./getStudyBlockTextFromUrl";
+import { diffLines, Change } from "diff";
 
 export default class GospelStudyPlugin extends Plugin {
 	public settings!: GospelStudyPluginSettings;
+
+	handledByPasteEvent!: boolean;
 
 	/**
 	 * Loads the plugin settings from the data store.
@@ -29,8 +32,14 @@ export default class GospelStudyPlugin extends Plugin {
 
 		this.addSettingTab(new GospelStudyPluginSettingTab(this.app, this));
 
+		this.handledByPasteEvent = false;
+
 		this.registerEvent(
 			this.app.workspace.on("editor-paste", this.onEditorPaste.bind(this))
+		);
+
+		this.registerEvent(
+			this.app.workspace.on("editor-change", this.onEditorChange.bind(this))
 		);
 	}
 
@@ -106,6 +115,8 @@ export default class GospelStudyPlugin extends Plugin {
 
 		const blockText = await getStudyBlockTextFromUrl(clipboardData, this.settings);
 
+		this.handledByPasteEvent = true;
+
 		editor.replaceSelection(blockText);
 
 		if (this.settings.copyCurrentNoteLinkAfterPaste === true) {
@@ -113,4 +124,34 @@ export default class GospelStudyPlugin extends Plugin {
 		}
 	}
 
+	private async onEditorChange(editor: Editor, info: { data: string; }) {
+		if (this.handledByPasteEvent){ 
+			// ensure we don't try to handle the paste event again
+			this.handledByPasteEvent = false;
+			return;
+		}
+
+		console.log("editor changed");
+
+		const previousContent = info.data;
+		const currentContent = editor.getValue();
+
+		if (previousContent !== currentContent) {
+			const diff = diffLines(previousContent, currentContent);
+			const addedLines = diff.filter((part: Change) => part.added);
+
+			const urlPattern = "https://www.churchofjesuschrist.org/study/";
+			const addedUrlLine = addedLines.find((part: Change) => part.value.includes(urlPattern));
+
+			if (addedUrlLine) {
+				const blockText = await getStudyBlockTextFromUrl(addedUrlLine.value, this.settings);
+				editor.replaceSelection(blockText);
+
+				if (this.settings.copyCurrentNoteLinkAfterPaste === true) {
+					this.copyCurrentNoteLinkToClipboard();
+				}
+			}
+		}
+		this.handledByPasteEvent = false;
+	}
 }
