427829016a6391276269aea60459ebd39d9e860d
Ky Kartchner <kartchnerky@gmail.com>
2024-05-15 19:51:27 -0600

    add documentation

--- src/models/ParagraphFormatter.ts
+++ src/models/ParagraphFormatter.ts
@@ -1,7 +1,21 @@
 import { GospelStudyPluginSettings } from "./GospelStudyPluginSettings";
 
+/**
+ * Represents a paragraph formatter.
+ */
 export interface ParagraphFormatter {
+
+    /**
+     * Determines if the formatter is enabled based on the settings.
+     * @param settings - The plugin settings.
+     * @returns A boolean indicating if the formatter is enabled.
+     */
     isEnabled(settings: GospelStudyPluginSettings): boolean;
 
+    /**
+     * Formats the paragraph.
+     * @param paragraph - The paragraph to be formatted.
+     * @returns The formatted paragraph.
+     */
     format(paragraph: string): string;
 }
