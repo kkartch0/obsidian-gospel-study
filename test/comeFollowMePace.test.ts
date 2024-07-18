a8e7908195e19343fca5cda5d7d21e989d8e2b1a
Ky Kartchner <kartchnerky@gmail.com>
2024-07-18 07:48:34 -0600

    progress

--- test/comeFollowMePace.test.ts
+++ test/comeFollowMePace.test.ts
@@ -18,14 +18,31 @@ async function getParagraphsFromUrl(urlToRequest: string) {
 		const parser = new DOMParser();
 		const sourceDocument = parser.parseFromString(response.text, "text/html");
 
+		const title = `Come Follow Me: ${sourceDocument.title}`;
+
 		// only select paragraphs with id matching the pattern "p\d+"
 		const paragraphs = Array.from(sourceDocument.querySelectorAll("p[id^=p]"));
 		const paragraphsByDay = divideIntoGroups(paragraphs);
 
 		// print out paragraph ids for each day
-		paragraphsByDay.forEach((day, i) => {
+		let oneTaskPerDay = false;
+
+		paragraphsByDay.forEach((dayParagraphs, i) => {
 			console.log(`Day ${i + 1}:`);
-			day.forEach(p => console.log(p.id));
+
+			if (oneTaskPerDay) {
+				const readingUrl = `"${urlToRequest}&id=${dayParagraphs[0].id}-${dayParagraphs[dayParagraphs.length - 1].id}#${dayParagraphs[0].id}"`;
+				console.log(`  Reading URL: ${readingUrl}`);
+			} else {
+				const dayTasks = dayParagraphs.map(paragraph => {
+					const paragraphUrl = `${urlToRequest}&id=${paragraph.id}#${paragraph.id}`;
+					const paragraphNumber = paragraph.id.replace("p", "");
+
+					return `- [ ] [${title} (${paragraphNumber}/${paragraphs.length})](${paragraphUrl})`;
+				});
+
+				console.log(dayTasks.join("\n"));
+			}
 		});
 }
 
@@ -36,10 +53,10 @@ function divideIntoGroups(paragraphs: Element[]) {
 
 	const paragraphsByDay = [];
 
-	for (let i = 0; i < 7; i++) {
-		const start = i * totalWholeParagraphsPerDay;
+	let start = 0;
+	let end = totalWholeParagraphsPerDay;
 
-		let end = Math.min((i + 1) * totalWholeParagraphsPerDay, totalParagraphs);
+	for (let i = 0; i < 7; i++) {
 		if (remainingExtraParagraphs > 0) {
 			end++;
 			remainingExtraParagraphs--;
@@ -47,6 +64,9 @@ function divideIntoGroups(paragraphs: Element[]) {
 
 		const dayParagraphs = paragraphs.slice(start, end);
 		paragraphsByDay.push(dayParagraphs);
+
+		start += (end - start);
+		end = Math.min(end + totalWholeParagraphsPerDay, totalParagraphs);
 	}
 
 	return paragraphsByDay;
