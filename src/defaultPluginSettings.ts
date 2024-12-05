526a2ee9e100d52322480b874cfbf2ca8edab7e3
Ky Kartchner <kartchnerky@gmail.com>
2024-12-05 16:41:27 -0700

    fix: correct spacing in STUDY_BLOCK_FORMAT_2 for improved formatting

--- src/defaultPluginSettings.ts
+++ src/defaultPluginSettings.ts
@@ -2,7 +2,7 @@ import { GospelStudyPluginSettings } from "./models/GospelStudyPluginSettings";
 
 export const STUDY_BLOCK_FORMAT_1 = "> [!gospel-study]\n> # {{referenceLink}}\n> {{paragraphs:\n>\n> }}\n>\n>{{tag}}";
 
-export const STUDY_BLOCK_FORMAT_2 = "> {{paragraphs:\n>\n>}}\n>\n> {{referenceLink}}\n\n{{tag}}";
+export const STUDY_BLOCK_FORMAT_2 = "> {{paragraphs:\n>\n> }}\n>\n> {{referenceLink}}\n\n{{tag}}";
 
 export const DEFAULT_SETTINGS: GospelStudyPluginSettings = {
 	studyBlockFormat: STUDY_BLOCK_FORMAT_1,
