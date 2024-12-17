024571b4c6f2e0c6f22770de540efdc1de5601ae
Ky Kartchner <46807501+kkartch0@users.noreply.github.com>
2024-12-17 12:45:20 +0000

    update

---
+++ .devcontainer/setup.sh
@@ -0,0 +1,3 @@
+OBSIDIAN_VERSION=1.7.7
+wget -O obsidian.deb "https://github.com/obsidianmd/obsidian-releases/releases/download/v${OBSIDIAN_VERSION}/obsidian_${OBSIDIAN_VERSION}_amd64.deb"
+dpkg -i obsidian.deb
